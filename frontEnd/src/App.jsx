d53820f6ca9cf5f16143ce354d31cb0221c15887
lokeshdongre93 <lokeshdongre93@gmail.com>
2025-12-30 17:39:21 +0530

    Add routing and navigation components for improved user experience

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,22 +1,34 @@
 // import AllCoursesCard from "./components/AllCoursesCard";
+import { Route, Routes } from "react-router-dom";
 import AddStudent from "./useComponnets/AddStudent";
 import Courses from "./useComponnets/Courses";
 import Students from "./useComponnets/Students";
+import NavBar from "./components/NavBar";
 
 function App() {
+  // return (
+  //   <>
+  //     <h1>
+  //       <br />
+  //       <u>Course List</u>
+  //     </h1>
+  //     <Courses />
+  //     <h1>
+  //       <br />
+  //       <u>Student List</u>
+  //     </h1>
+
+  //     <Students />
+  //   </>
+  // );
   return (
     <>
-      <h1>
-        <br />
-        <u>Course List</u>
-      </h1>
-      <Courses />
-      <h1>
-        <br />
-        <u>Student List</u>
-      </h1>
-
-      <Students />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Courses />} />
+        <Route path="/students" element={<Students />} />
+        <Route path="/add-student" element={<AddStudent />} />
+      </Routes>
     </>
   );
 }
